c76295374be2739d6cd42af2594bd92bf0ea3821
Steven Pumel <spumel534@gmail.com>
2023-11-18 22:27:14 -0500

    Login and Create account page skeleton

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,37 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './Login.css';
 
 const Login = () => {
     return (
-        <div className="hdr">
-            <button></button>
-            <h1>THIS LOGS U IN</h1>
+        <div>
+            <div className="hdr">
+                <Link to='/'>Home</Link>
+                <h1>Login Page</h1>
+            </div>
+            <div class='card login'>
+                <form>
+                    <label>Title</label>
+                    <input
+                        id="title"
+                        type="text"
+                    />
+                    <label>Date</label>
+                    <input
+                        id="date"
+                        type="text"
+                    />
+                    <label>Comment</label>
+                    <input
+                        id="comment"
+                        type="text"
+                    />
 
+                    <button class="buttonEdit" type="button">Login</button>
+                    <Link to='/signup'>Dont have an account? Sign up</Link>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/Login/LoginCreate.js
+++ src/components/Login/LoginCreate.js
@@ -1,14 +1,37 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './Login.css';
 
 const LoginCreate = () => {
     return (
-        <div className="hdr">
-            <button></button>
-            <h1>THIS IS FOR CREATING ACCOUNT</h1>
+        <div>
+            <div className="hdr">
+                <Link to='/'>Home</Link>
+                <h1>Create your account</h1>
+                <Link to='/login'>Login</Link>
+            </div>
+            <div class='card login'>
+                <form>
+                    <label>Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                    />
+                    <label>Email</label>
+                    <input
+                        id="email"
+                        type="text"
+                    />
+                    <label>Password</label>
+                    <input
+                        id="password"
+                        type="text"
+                    />
 
+                    <button class="buttonEdit" type="button">Create</button>
+                </form>
+            </div>
         </div>
     );
 };
